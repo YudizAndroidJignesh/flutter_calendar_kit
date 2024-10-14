ea00ec1ba5ae6ff2db1ba671e187c1d328870bc1
Jignesh Shakya <JIGNESH.SHAKYA1997PRO@GMAIL.COM>
2024-10-14 17:06:53 +0530

    Update flutter_custom_calendar_test.dart

--- test/flutter_custom_calendar_test.dart
+++ test/flutter_custom_calendar_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_custom_calendar/flutter_custom_calendar.dart';
+import 'package:flutter_calendar_kit/flutter_custom_calendar.dart';
 
 void main() {
   test('adds one to input values', () {
